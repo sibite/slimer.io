e3c901e50a53350ed62fd5b9526454e6ed53c87e
DavRei <davreiyt@gmail.com>
2019-10-22 20:53:28 +0200

    Add slime splitting and make some improvements

--- slimer.io.js
+++ slimer.io.js
@@ -11,14 +11,15 @@ let Animated = function(value)  {
 }
 
 let ease = new BezierCurve(30, 3, [0, 0], [0.5, 0], [0.5, 1], [1, 1]);
+let easeOut = new BezierCurve(30, 3, [0, 0], [0, 0], [0.5, 1], [1, 1]);
 
-Animated.prototype.animate = function(valueChange, time)  {
+Animated.prototype.animate = function(valueChange, time, timingFunction = ease)  {
   let i = 1;
   let maxI = Math.round(time / (1000 / game.frameRate));
   this.animCount += 1;
   let frame = function()  {
     if (maxI > 1)  {
-      this.value += valueChange * (    ease.getYofX(i / (maxI-1)) - ease.getYofX((i-1) / (maxI-1))   );
+      this.value += valueChange * (    timingFunction.getYofX(i / (maxI-1)) - timingFunction.getYofX((i-1) / (maxI-1))   );
     }  else  {
       this.value += valueChange;
     }
@@ -31,25 +32,59 @@ Animated.prototype.animate = function(valueChange, time)  {
 }
 
 
-let Slime = function(diameter, color, borderColor, x, y)  {
-	this.diameter =	diameter;
+let SlimePart = function(diameter, x, y, xExtraSpeed = 0, yExtraSpeed = 0, xMove = 0, yMove = 0)  {
+  this.diameter =	diameter;
+  this.diameterAnimated = new Animated(diameter);
   this.score = Math.floor(Math.pow(diameter/2, 2) * Math.PI / 100);
-	this.color = color;
-	this.borderColor = borderColor;
-	this.speed = Math.pow(this.diameter, 10/17) / this.diameter * 20;
+  this.speed = Math.pow(this.diameter, 10/17) / this.diameter * 20;
+  this.xExtraSpeed = xExtraSpeed;
+  this.yExtraSpeed = yExtraSpeed;
 	this.x = x;
 	this.y = y;
-  this.directionX = 0;
-  this.directionY = 0;
-  this.lastPosUpdate = Date.now();
-  this.animatedDiameter = new Animated(diameter);
+  this.xMove = xMove;
+  this.yMove = yMove;
 
   this.updateDiameter = function(diameter)  {
-    this.animatedDiameter.animate(diameter - this.diameter, 400);
+    this.diameterAnimated.animate(diameter - this.diameter, 700, easeOut);
     this.diameter = diameter;
     this.score = Math.floor(Math.pow(diameter/2, 2) * Math.PI / 100);
-    this.speed = Math.pow(this.diameter, 10/17) / this.diameter * 20;
-  }
+    this.speed = 25 / Math.pow(this.diameter, 10/25);
+  };
+
+  this.reduceTimeToSplit = function(timeMS)  {
+    let time = timeMS / 1000;
+    this.timeToSplit -= time / Math.max(1, this.diameter / 1000);
+  };
+}
+
+
+let Slime = function(diameter, color, borderColor, x, y)  {
+  this.parts = [new SlimePart(diameter, x, y)];
+  this.score = Math.floor(Math.pow(diameter/2, 2) * Math.PI / 100);
+  this.diameterSum = diameter;
+	this.color = color;
+	this.borderColor = borderColor;
+  this.mergeTimeLeft = 0;
+  this.lastPosUpdate = Date.now();
+  this.updateScore = function()  {
+    let sqArea = this.parts.reduce((total, next) => total + Math.pow(next.diameter/2, 2), 0);
+    this.score = Math.floor(sqArea * Math.PI / 100);
+    this.diameterSum = Math.sqrt(sqArea) * 2;
+  };
+  this.split = function()  {
+    if (this.parts.length < game.playerMaxParts)  {
+      this.mergeTimeLeft = game.mergeTime;
+      this.parts.filter(part => part.score >= game.minSplitScore).forEach(function(part)  {
+        if (this.parts.length < game.playerMaxParts)  {
+          part.updateDiameter(Math.sqrt(Math.pow(part.diameter, 2) / 2));
+          this.parts.push(new SlimePart(
+            part.diameter, part.x, part.y,
+            part.xMove + part.mouseXfactor * game.splitSpeed,    part.yMove + part.mouseYfactor * game.splitSpeed,
+            part.xMove, part.yMove));
+        }
+      }.bind(this));
+    }
+  };
 }
 
 
@@ -80,6 +115,8 @@ let Canvas = function(game) {
     this.el.style.width = width + "px";
     this.el.style.height = height + "px";
   };
+
+  this.resize();
 
   this.clear = function()  {
     this.ctx.clearRect(0, 0, this.width, this.height);
@@ -124,12 +161,16 @@ let Canvas = function(game) {
   };
 
   this.drawPlayerScore = function(score)  {
-    let rPCoords = this.game.convertCoords(this.game.player.x, this.game.player.y);
-    this.ctx.font = "bold 30px Ubuntu";
+    let rPCoords = this.game.convertCoords(
+      this.game.player.parts[this.game.player.parts.length-1].x,
+      this.game.player.parts[this.game.player.parts.length-1].y
+    );
+    let fontSize = 24;
+    this.ctx.font = "bold "+fontSize+"px Ubuntu, sans-serif";
     this.ctx.textAlign = "center";
     this.ctx.textBaseline = "middle";
     this.ctx.strokeStyle = "rgba(0, 0, 0, 0.3)";
-    this.ctx.lineWidth = 5;
+    this.ctx.lineWidth = fontSize / 6;
     this.ctx.lineJoin = "round";
     this.ctx.fillStyle = "rgb(255, 255, 255)";
     this.ctx.strokeText(score, rPCoords[0], rPCoords[1]);
@@ -141,6 +182,13 @@ let Canvas = function(game) {
 let Game = function()  {
   this.canvas = new Canvas(this);
   this.frameRate = 60;
+  this.playerStartDiameter = 40;
+  this.playerMaxParts = 16;
+  this.minSplitScore = 60;
+  this.splitSpeed = 12;
+  this.mergeTime = 25;
+  this.mergeTimeGrow = 0.08;
+  this.partsRepulsingSpeed = 20;
   this.noFoodSpawningTime = 0;
   this.borderWidth = 7;
 	this.foodDiameter = 22;
@@ -156,14 +204,14 @@ let Game = function()  {
 	this.scoreOutlineSize = 2;
   this.camX = this.mapWidth / 2;
   this.camY = this.mapHeight / 2;
+  this.foods = [];
   let playerColor = randNum(0, 360);
   this.player = new Slime(
-                40,
+                this.playerStartDiameter,
                 "hsl("+playerColor+", 100%, 50%)",
                 "hsl("+playerColor+", 100%, 40%)",
                 this.mapWidth / 2,
                 this.mapHeight / 2);
-  this.foods = [];
 
   this.convertCoords = function(x, y)  {
     let camLeft = this.camX - this.canvas.width / 2 / this.viewScaleAnimated.value;
@@ -172,8 +220,7 @@ let Game = function()  {
     let canvasY = (y - camTop) * this.viewScaleAnimated.value;
     return [canvasX, canvasY];
   };
-
-  let mouseC = this.convertCoords(this.mapWidth / 2, this.mapHeight / 2);
+  let mouseC = this.convertCoords(this.mapWidth / 2 + 0.0001, this.mapHeight / 2 + 0.0001);
 	this.mouseX = mouseC[0];
 	this.mouseY = mouseC[1];
 
@@ -251,67 +298,143 @@ Game.prototype.render = function()  {
   //EATING FOODS
 
 
-  this.foods.filter(function(food)  {
-    let playerRadius = this.player.diameter / 2 + this.borderWidth;
-    let distance = Math.sqrt(
-      Math.pow(Math.abs(food.x - this.player.x), 2) +
-      Math.pow(Math.abs(food.y - this.player.y), 2));
-    return distance < playerRadius;
-  }.bind(this)).forEach(function(food)  {
-    this.player.updateDiameter(Math.sqrt(Math.pow(this.player.diameter, 2) + food.kcal));
-    this.foods.splice(this.foods.indexOf(food), 1);
+  this.player.parts.forEach(function(part) {
+    this.foods.filter(function(food)  {
+      let partRadius = part.diameter / 2 + this.borderWidth;
+      let distance = Math.sqrt(
+        Math.pow(Math.abs(food.x - part.x), 2) +
+        Math.pow(Math.abs(food.y - part.y), 2));
+      return distance < partRadius;
+    }.bind(this)).forEach(function(food)  {
+      part.updateDiameter(Math.sqrt(Math.pow(part.diameter, 2) + food.kcal));
+      this.foods.splice(this.foods.indexOf(food), 1);
+    }.bind(this));
   }.bind(this));
 
+  this.player.parts.sort((a, b) => a.diameter - b.diameter);
+  this.player.updateScore();
 
-  //UPDATE PLAYER POSITION
+
+  //UPDATE PLAYER POSITIONS
   {
+    let timeDifference = Date.now() - this.player.lastPosUpdate;
 
-  let playerCanvasCoords = this.convertCoords(this.player.x, this.player.y);
-  let mouseXoffset = this.mouseX - playerCanvasCoords[0];
-  let mouseYoffset = this.mouseY - playerCanvasCoords[1];
-  let preDistance = Math.sqrt(Math.pow(mouseXoffset, 2) + Math.pow(mouseYoffset, 2));
-  let distance = preDistance < 100 ? 100 : preDistance;
+    this.player.parts.forEach(function(part) {
 
-  if (preDistance > 10)  {
-    now = Date.now();
-    playerXmove = (mouseXoffset / distance) * this.player.speed * (now - this.player.lastPosUpdate) / 15;
-    playerYmove = (mouseYoffset / distance) * this.player.speed * (now - this.player.lastPosUpdate) / 15;
+      let partCanvasCoords = this.convertCoords(part.x, part.y);
+      let mouseXoffset = this.mouseX - partCanvasCoords[0];
+      let mouseYoffset = this.mouseY - partCanvasCoords[1];
+      let preDistance = Math.sqrt(Math.pow(mouseXoffset, 2) + Math.pow(mouseYoffset, 2));
+      let distance = preDistance < 100 ? 100 : preDistance;
+      part.mouseXfactor = mouseXoffset / preDistance;
+      part.mouseYfactor = mouseYoffset / preDistance;
 
-    if (Math.abs(playerXmove) < Math.abs(mouseXoffset / this.viewScaleAnimated.value))  {
-      this.player.x += playerXmove;
-    }  else  {
-      this.player.x += mouseXoffset / this.viewScaleAnimated.value;
-    }
-    if (Math.abs(playerYmove) < Math.abs(mouseYoffset / this.viewScaleAnimated.value))  {
-      this.player.y += playerYmove;
-    }  else  {
-      this.player.y += mouseYoffset / this.viewScaleAnimated.value;
-    }
-  }
 
-  this.player.x = this.player.x < 0 ? 0 : this.player.x > this.mapWidth ? this.mapWidth : this.player.x;
-  this.player.y = this.player.y < 0 ? 0 : this.player.y > this.mapHeight ? this.mapHeight : this.player.y;
+      if (preDistance > 10)  {
+        part.xMove = (mouseXoffset / distance) * part.speed * timeDifference / 15;
+        part.yMove = (mouseYoffset / distance) * part.speed * timeDifference / 15;
 
-  this.player.lastPosUpdate = Date.now();
-  this.camX = this.player.x;
-  this.camY = this.player.y;
+        part.x += part.xMove + part.xExtraSpeed;
+        part.y += part.yMove + part.yExtraSpeed;
+      }
+      else  {
+        part.x += part.xExtraSpeed;
+        part.y += part.yExtraSpeed;
+      }
+
+      let esDistance = Math.sqrt(Math.pow(part.xExtraSpeed, 2) + Math.pow(part.yExtraSpeed, 2))
+
+      if (part.xExtraSpeed > 0)        part.xExtraSpeed = Math.max(part.xExtraSpeed - (part.xExtraSpeed / esDistance * timeDifference / 100), 0);
+      else if (part.xExtraSpeed < 0)   part.xExtraSpeed = Math.min(part.xExtraSpeed - (part.xExtraSpeed / esDistance * timeDifference / 100), 0);
+
+      if (part.yExtraSpeed > 0)        part.yExtraSpeed = Math.max(part.yExtraSpeed - (part.yExtraSpeed / esDistance * timeDifference / 100), 0);
+      else if (part.yExtraSpeed < 0)   part.yExtraSpeed = Math.min(part.yExtraSpeed - (part.yExtraSpeed / esDistance * timeDifference / 100), 0);
+
+      let xBefore = part.x;
+      let yBefore = part.y;
+
+      part.x = part.x < 0 ? 0 : part.x > this.mapWidth ? this.mapWidth : part.x;
+      part.y = part.y < 0 ? 0 : part.y > this.mapHeight ? this.mapHeight : part.y;
+
+      part.xMove -= xBefore - part.x;
+      part.yMove -= yBefore - part.y;
+
+    }.bind(this));
+
+    //PARTS MERGING
+
+    this.player.mergeTimeLeft -= timeDifference / 1000;
+
+    let mergingParts = this.player.parts.filter(part => this.player.mergeTimeLeft + part.diameter * this.mergeTimeGrow <= 0)
+                                        .sort((a, b) => b.diameter - a.diameter);
+    mergingParts.forEach(function(part, index) {
+      let otherMergingParts = mergingParts.filter(otherPart => part != otherPart && otherPart.diameter <= part.diameter);
+      otherMergingParts.forEach(function(otherPart, otherIndex) {
+        let distance = Math.sqrt(Math.pow(part.x - otherPart.x, 2) + Math.pow(part.y - otherPart.y, 2));
+        let distanceBetween = distance - (part.diameter + otherPart.diameter) / 2 - this.borderWidth*2;
+        if (-distanceBetween > otherPart.diameter * 2/3)  {
+          part.updateDiameter(Math.sqrt(Math.pow(part.diameter, 2) + Math.pow(otherPart.diameter, 2)));
+          this.player.parts.splice(this.player.parts.indexOf(otherPart), 1);
+          mergingParts.splice(index, 1);
+          otherMergingParts.splice(otherIndex, 1);
+        }
+      }.bind(this));
+    }.bind(this));
+
+    //PARTS REPULSING
+
+    this.player.parts.filter(part => mergingParts.indexOf(part) == -1)
+    .forEach(function(part)  {
+      this.player.parts.filter(otherPart => part != otherPart)
+      .forEach(function(otherPart)  {
+        let distance = Math.sqrt(Math.pow(part.x - otherPart.x, 2) + Math.pow(part.y - otherPart.y, 2));
+        let distanceBetween = distance - (part.diameter + otherPart.diameter) / 2 - this.borderWidth*2;
+        if (distanceBetween < 0 && distance != 0)  {
+          let xFactor = (part.x - otherPart.x) / distance;
+          let yFactor = (part.y - otherPart.y) / distance;
+          let xDifference = xFactor * (-distanceBetween);
+          let yDifference = yFactor * (-distanceBetween);
+          let xChange = xFactor * this.partsRepulsingSpeed / 2;
+          let yChange = yFactor * this.partsRepulsingSpeed / 2;
+          if (Math.abs(xChange) > Math.abs(xDifference / 2))  {
+            xChange = xDifference / 2;
+          }
+          if (Math.abs(yChange) > Math.abs(yDifference / 2))  {
+            yChange = yDifference / 2;
+          }
+          let totalSpeed = part.speed + otherPart.speed;
+          part.x += xChange * part.speed / totalSpeed;
+          part.y += yChange * part.speed / totalSpeed;
+          otherPart.x -= xChange * otherPart.speed / totalSpeed;
+          otherPart.y -= yChange * otherPart.speed / totalSpeed;
+        }
+      }.bind(this));
+    }.bind(this));
+
+    this.player.lastPosUpdate = Date.now();
+    this.camX = this.player.parts.reduce((total, next) => total + next.x, 0) / this.player.parts.length;
+    this.camY = this.player.parts.reduce((total, next) => total + next.y, 0) / this.player.parts.length;
+
 
   }
   //RENDER PLAYER
   {
 
-  let canvasCoords = this.convertCoords(this.player.x, this.player.y);
-  this.canvas.drawSlime(this.player.animatedDiameter.value, this.player.color, this.player.borderColor, canvasCoords[0], canvasCoords[1]);
-  this.canvas.drawPlayerScore(this.player.score);
+    this.player.parts.forEach(function(part) {
+      let canvasCoords = this.convertCoords(part.x, part.y);
+      this.canvas.drawSlime(part.diameterAnimated.value, this.player.color, this.player.borderColor, canvasCoords[0], canvasCoords[1]);
+    }.bind(this));
+    this.canvas.drawPlayerScore(this.player.score);
 
   }
   //UPDATE VIEW SCALE
   {
 
-  let minSize = Math.min(this.canvas.width, this.canvas.height);
-  let newViewScale = Math.min(  3 / Math.pow(this.player.diameter, 10/40),			minSize / this.player.diameter / 2  );
-  this.viewScaleAnimated.animate(newViewScale - this.viewScale, 400);
-  this.viewScale = newViewScale;
+    let minSize = Math.min(this.canvas.width, this.canvas.height);
+    let newViewScale = Math.min(  3 / Math.pow(this.player.diameterSum, 10/40),			minSize / this.player.diameterSum / 2  )
+     * Math.pow(0.9, Math.log(this.player.parts.length));
+    this.viewScaleAnimated.animate(newViewScale - this.viewScale, 700, ease);
+    this.viewScale = newViewScale;
 
   }
 
@@ -333,19 +456,23 @@ game.render();
 
 
 
-let touchCount = 0;
+let touchCount = 0, focusedOnCanvas = true;
+canvasFocusCheck = function(target)  {
+  focusedOnCanvas = target == game.canvas.el;
+}
 
 game.canvas.el.addEventListener("mousemove", function(event) {
   game.mouseX = event.clientX;
   game.mouseY = event.clientY;
 });
-
-game.canvas.el.addEventListener("touchmove", function(event) {
-	if (event.touches.length < 2)  {
-		event.preventDefault();
-  }
-  game.mouseX = event.touches[0].clientX;
-  game.mouseY = event.touches[0].clientY;
+document.addEventListener("click", function(event)  {
+  canvasFocusCheck(event.target);
+});
+document.addEventListener("touchstart", function(event) {
+  canvasFocusCheck(event.target);
+});
+game.canvas.el.addEventListener("dblclick", function() {
+  darkMode++;
 });
 game.canvas.el.addEventListener("touchstart", function(event) {
   touchCount += event.changedTouches.length;
@@ -361,14 +488,25 @@ game.canvas.el.addEventListener("touchstart", function(event) {
     darkMode++;
   }
 });
+game.canvas.el.addEventListener("touchmove", function(event) {
+	if (event.touches.length < 2)  {
+		event.preventDefault();
+  }
+  game.mouseX = event.touches[0].clientX;
+  game.mouseY = event.touches[0].clientY;
+});
 game.canvas.el.addEventListener("touchend", function(event)  {
   touchCount -= event.changedTouches.length;
 });
-game.canvas.el.addEventListener("dblclick", function() {
-  darkMode++;
-});
 window.addEventListener("resize", function()  {
 	game.canvas.resize();
+});
+document.addEventListener("keydown", function(event) {
+  if (focusedOnCanvas)  {
+    if (event.key == " ") {
+      game.player.split();
+    }
+  }
 });
 
 
